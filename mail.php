9413a1bea8fa360d9f16a1c4a03e42a5673a0357
iraneira <iraneira@users.noreply.github.com>
2017-10-09 15:19:05 +0400

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,62 @@
+﻿ 
+<? 
+// ----------------------------конфигурация-------------------------- // 
+ 
+$adminemail="ira-neira@mail.ru";  // e-mail админа 
+ 
+ 
+$date=date("d.m.y"); // число.месяц.год 
+ 
+$time=date("H:i"); // часы:минуты:секунды 
+ 
+$backurl="//localhost/Users/s0ber/Documents/stomatologia/index.html";  // На какую страничку переходит после отправки письма 
+ 
+//---------------------------------------------------------------------- // 
+ 
+  
+ 
+// Принимаем данные с формы 
+ 
+$name=$_POST['name']; 
+ 
+$tel=$_POST['tel']; 
+ 
+$date=$_POST['date']; 
+ 
+ 
+ 
+$msg=" 
+ 
+ 
+<p>Имя: $name</p> 
+ 
+ 
+<p>E-mail: $tel</p> 
+ 
+ 
+<p>Дата: $date</p> 
+ 
+ 
+"; 
+ 
+  
+ 
+ // Отправляем письмо админу  
+ 
+mail("$adminemail", "$date $time Сообщение 
+от $name", "$msg"); 
+ 
+  
+// Выводим сообщение пользователю 
+ 
+print "<script language='Javascript'><!-- 
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); 
+//--></script> 
+ 
+$msg 
+ 
+<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
+exit; 
+ 
+ 
+?>
